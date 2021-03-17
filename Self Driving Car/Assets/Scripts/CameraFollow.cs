9270dde720ef366a5b10f4683c554c2e723a07f4
BuildTools <unconfigured@null.spigotmc.org>
2021-03-17 09:32:35 -0700

    Added car movement

---
+++ Self Driving Car/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform playerPos;
+
+    private void Awake() {
+        playerPos = GameObject.Find("Car").GetComponent<Transform>();
+    }
+
+    private void Update() {
+        transform.position = new Vector3(playerPos.position.x, playerPos.position.y, -10);
+    }
+}
---
+++ Self Driving Car/Assets/Scripts/CarScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour
+{
+    private Transform spawnPosition;
+    public float speed, turningSpeed;
+
+    private void Awake() {
+        spawnPosition = GameObject.Find("Spawn").GetComponent<Transform>();
+    }
+
+    private void Update() {
+        transform.position += transform.right * speed * Time.deltaTime;
+    }
+
+    private void FixedUpdate() {
+        float rot = Input.GetAxisRaw("Horizontal");
+        transform.rotation = Quaternion.Euler(0, 0, transform.eulerAngles.z + (-rot * turningSpeed));
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "wall") {
+            transform.position = spawnPosition.position;
+        }
+    }
+}
